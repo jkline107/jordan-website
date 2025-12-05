07b2a4df6f6209d11b5476268922fbb9f8862587
Jordan Klein <jek78000@gmail.com>
2025-12-05 15:53:02 -0500

    Adding embedded youtube video

--- src/pages/ColoristReel.jsx
+++ src/pages/ColoristReel.jsx
@@ -1,20 +1,20 @@
- function ColoristReel() {
+function ColoristReel() {
   return (
     <section className="page page-dark">
       <div className="container">
         <h1 className="giant-heading">COLORIST REEL</h1>
+
         <p className="page-lead">
           Please set the video to 1080p for the best viewing experience!
         </p>
 
         <div className="video-wrapper">
-          {/* TODO: replace VIDEO_ID_HERE with your actual YouTube ID */}
           <iframe
             className="video-iframe"
-            src="https://www.youtube.com/embed/VIDEO_ID_HERE"
-            title="Colorist reel"
+            src="https://www.youtube.com/embed/t-W9rVVl4CI"
+            title="Jordan Klein Colorist Reel"
             frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen
           ></iframe>
         </div>
